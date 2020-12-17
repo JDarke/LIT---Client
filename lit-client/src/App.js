25e9b00ad4c14c0a821f77edc5fc97d32fb875da
John Darke <johndarke@live.co.uk>
2020-12-17 17:39:57 +0000

    add socket.io, functional, basic chat form

--- lit-client/src/App.js
+++ lit-client/src/App.js
@@ -1,23 +1,50 @@
-import logo from './logo.svg';
+import React, { useState, useEffect } from 'react';
+import io from 'socket.io-client';
 import './App.css';
 
-function App() {
+const ENDPOINT = 'localhost:8080';
+const socket = io(ENDPOINT);
+
+const App = () => {
+  const [test, setTest] = useState('test');
+  const [text, setText] = useState('text');
+      
+  useEffect(() => {
+    socket.on('send_message', function(msg) {
+      console.log('receiving msg: ' + msg)
+    });
+  }, [text]);
+
+  // socket.on('connect', function(data) {
+  //   //console.log(socket.client.id);
+  //   //socket.emit('join', 'New user has joined');
+  // });
+  
+  socket.on('reply', function(msg) {
+    console.log('received: ' + msg)
+  });
+
+  const handleChange = (e) => {
+    let change = e.target.value
+    setTest(change);
+    //console.log(change)
+  } 
+
+  const sendMessage = (msg) => {
+    //console.log(socket);
+    console.log('send');
+    setText(msg)
+    socket.emit('send_message', msg);
+    setTest('');
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <ul id="messages"></ul>
+      <form action="" onSubmit={(e) => e.preventDefault()}>
+      <input id="m" autoComplete="off" value={test} onChange={(e) => handleChange(e)} />
+      <button onClick={()=>sendMessage(test)}>Send</button>
+      </form>
     </div>
   );
 }
