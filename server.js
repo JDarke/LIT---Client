cde19ac88b4f888f8308b520ab57807881b559e5
John Darke <johndarke@live.co.uk>
2020-12-30 11:41:08 +0000

    debugging

--- server.js
+++ server.js
@@ -176,7 +176,7 @@ io.on("connection", (socket) => {
       console.log(name + " logged in");
     }
     
-    console.log('users: ' + users);
+    console.log('users: ', ...users);
   });
 
   socket.on("create", ({ name, userRoom }, roomNameIsTaken) => {
@@ -198,8 +198,8 @@ io.on("connection", (socket) => {
         io.emit("roomInfo", rooms);
         io.sockets.in(userRoom).emit("usersInRoom", getUsersInRoom(userRoom));
         console.log(name +  " created " + userRoom);
-        console.log("users: ",  users);
-        console.log("rooms: ",  rooms);
+        console.log("users: ",  ...users);
+        console.log("rooms: ",  ...rooms);
         //console.log(io.sockets.adapter.rooms);
       }
     }
@@ -208,7 +208,7 @@ io.on("connection", (socket) => {
   socket.on("join", ({ name, userRoom }, roomNotFound) => {
     getRooms();
     let existingRoom = rooms.find((room) => room === userRoom);
-    //console.log("rooms: ",  rooms);
+    //console.log("rooms: ",  ...rooms);
     
     if (!existingRoom) {
       roomNotFound(true);
@@ -231,8 +231,8 @@ io.on("connection", (socket) => {
         getRooms();
         io.sockets.in(userRoom).emit("usersInRoom", getUsersInRoom(userRoom));
         console.log(name +  " joined " + userRoom);
-        console.log("users: ",  users);
-        console.log("rooms: ",  rooms);
+        console.log("users: ",  ...users);
+        console.log("rooms: ",  ...rooms);
       }
     }
   });
@@ -262,15 +262,15 @@ io.on("connection", (socket) => {
 
     io.sockets.in(room).emit("usersInRoom", getUsersInRoom(room)); // was userRoom  !?  // FIXED also, shouldnt this only be sent to that room?  This seems to broadcast to all rooms and may overwrite their lists
     console.log(name +  " left " + room);
-    console.log("users: ",  users);
-    console.log("rooms: ",  rooms);
+    console.log("users: ",  ...users);
+    console.log("rooms: ",  ...rooms);
   });
 
   socket.on("logout", () => {
     let user = getUserById(socket.client.id);
     console.log( user.name +  " logged out");
     deleteUser(socket.client.id); // need some kind of tidy-up here, surely?
-    console.log("users:" + users);
+    console.log("users:", ...users);
     getRooms();
   });
 
@@ -279,8 +279,8 @@ io.on("connection", (socket) => {
     //getRooms();
 
     console.log("user disconnected" + getTime());
-    console.log("users: ",  users);
-    console.log("rooms: ",  rooms);
+    console.log("users: ",  ...users);
+    console.log("rooms: ",  ...rooms);
   });
 
   socket.on("reconnect", () => {
@@ -298,7 +298,7 @@ io.on("connection", (socket) => {
     getRooms();
 
     console.log(user.name + "reconnected" + getTime());
-    console.log("users: ",  users);
+    console.log("users: ",  ...users);
   });
 });
 
